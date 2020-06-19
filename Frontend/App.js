244119819c6bcf365c2f938161028b5881ac128a
kajalsawale <kajalsawale28@gmail.com>
2020-06-19 23:07:39 +0530

    Edited and linked Heading.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Image,  StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/login.png';
+import Heading from './Heading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-	<Image source={logo} style={styles.login} />
-	  <Text> Login Id : </Text>
-	  <Text> Password : </Text>
-	  <TouchableOpacity 
-	  onPress={ () => alert('Login Successful')} style={styles.button}>
-	  <Text style={styles.buttonText}>Login</Text>
-	  </TouchableOpacity>
+    <View style={{flex:1, padding:5 }}>
+		<Heading/>
     </View>
   );
 }
@@ -21,23 +16,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+	justifyContent: 'center',
+	margin: 0,
   },
   
-	login: {
-		width: 100,
-		height: 100,
-		marginTop: 50,
-	},
-	
-	button: {
-		backgroundColor: "green",
-		padding: 20,
-		borderRadius: 5,
-	},
-	
-	buttonText:{
-		fontSize: 20,
-		color: '#fff',
-	},
 });

e4488d5ef7b5923d0df36c7887209286da2e2f56
Prashant Dalvi <dalviprashant0907@gmail.com>
2020-06-21 18:14:32 +0530

    Header Template added

---
+++ Frontend/MyHeader.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Image,  StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Header} from 'react-native-elements';
+import home from './assets/home.png';
+
+
+class MyHeader extends Component
+{
+    render() {
+        return (
+            <Header
+            leftComponent={{ icon:{home}, color: '#fff' }}
+            centerComponent={{ text: 'COVID-19 ', style: { color: '#fff' } }}
+            rightComponent={{ color: '#fff' }}
+          />
+        );
+    }
+}
+
+export default MyHeader;
+

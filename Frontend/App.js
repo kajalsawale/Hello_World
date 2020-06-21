7682b16138082486ef0e7f30e5617c64b641bfdf
Prashant Dalvi <dalviprashant0907@gmail.com>
2020-06-21 12:43:50 +0530

    Removing Node_modules

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,43 +1,68 @@
 import React from 'react';
 import { Image,  StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/login.png';
+import styles from './styles'
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
+const users = [
+	{
+	   name: 'brynn',
+	   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+	}
+   ]
 export default function App() {
   return (
     <View style={styles.container}>
-	<Image source={logo} style={styles.login} />
-	  <Text> Login Id : </Text>
-	  <Text> Password : </Text>
-	  <TouchableOpacity 
-	  onPress={ () => alert('Login Successful')} style={styles.button}>
-	  <Text style={styles.buttonText}>Login</Text>
-	  </TouchableOpacity>
-    </View>
+
+	
+	   
+	
+	   <Card title="CARD WITH DIVIDER">
+		 {
+		   users.map((u, i) => {
+			 return (
+			   <View key={i} style={styles.user}>
+				 <Image
+				   style={styles.image}
+				   resizeMode="cover"
+				   source={{ uri: u.avatar }}
+				 />
+				 <Text style={styles.name}>{u.name}</Text>
+			   </View>
+			 );
+		   })
+		 }
+	   </Card>
+	   
+		<Card containerStyle={{padding: 0}} >
+		 {
+		   users.map((u, i) => {
+			 return (
+			   <ListItem
+				 key={i}
+				 roundAvatar
+				 title={u.name}
+				 avatar={{uri:u.avatar}}
+			   />
+			 );
+		   })
+		 }
+	   </Card>
+	   
+	   
+	   <Card
+		 title='HELLO WORLD'
+		 image source = {logo}>
+		 <Text style={{marginBottom: 10}}>
+		   The idea with React Native Elements is more about component structure than actual design.
+		 </Text>
+		 <Button
+		   icon={<Icon name='code' color='#ffffff' />}
+		   buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+		   title='VIEW NOW' />
+	   </Card>
+	   </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-	login: {
-		width: 100,
-		height: 100,
-		marginTop: 50,
-	},
-	
-	button: {
-		backgroundColor: "green",
-		padding: 20,
-		borderRadius: 5,
-	},
-	
-	buttonText:{
-		fontSize: 20,
-		color: '#fff',
-	},
-});
+
